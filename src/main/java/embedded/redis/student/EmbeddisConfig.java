36d5586d906dc0cc0103610981e8a40d865be383
Eric Johnson <eric.johnson@Astontech.com>
2019-04-17 12:56:36 -0500

    Initial commit

---
+++ src/main/java/embedded/redis/student/EmbeddisConfig.java
@@ -0,0 +1,29 @@
+package embedded.redis.student;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Component
+public class EmbeddisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+}
